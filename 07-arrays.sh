23654f33a9606a8a4fa1b1246fca980ff8150d7c
ArunramRajasekaran <ramarun04@gmail.com>
2025-06-02 13:05:05 +0530

    data type

---
+++ 07-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DEVOPSTOPICS=("ShellSchript", "Terraform", "Github")
+# index starts from 0, size is 3
+
+echo "First Topic: ${DEVOPSTOPICS[0]}"
+
+echo "All Topics are: ${DEVOPSTOPICS[@]}"
