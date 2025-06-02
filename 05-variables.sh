73a686f4f8fb85f6b6c54efad3d828843474c3ac
ArunramRajasekaran <ramarun04@gmail.com>
2025-06-02 11:29:13 +0530

    Password added

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter username:" #text entered here will be added as value to variable
+
+read USERNAME #Here USERNAME is the variable name
+
+echo "username entered: $USERNAME"
+
+echo "Please enter Passsword:"
+
+read -s Password
