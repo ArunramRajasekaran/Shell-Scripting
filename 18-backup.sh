913a5ada101d0265fd5c11127af0e39357ce1c99
ArunramRajasekaran <ramarun04@gmail.com>
2025-06-13 15:42:31 +0530

    Shell-scripting

--- 18-backup.sh
+++ 18-backup.sh
@@ -51,6 +51,6 @@ fi
 
 echo "Script started executing at; $TIMESTAMP" &>>$LOG_FILE_NAME
 
-FILES=$(find <$SOURCE_DIR> -name "*.log" +mtime <days>)
+FILES=$(find <$SOURCE_DIR> -name "*.log" -mtime +$DAYS)
 
 echo "Files are: $FILES"
